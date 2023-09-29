b9069df24993e1538865194d969e9298e81c0976
metacrist <BogdanSir1807@gmail.com>
2023-09-30 01:25:16 +0300

    test

---
+++ blog/message/models.py
@@ -0,0 +1,21 @@
+
+from django.db import models
+
+
+# class Message(models.Model):
+#     text = models.TextField()
+#     date = models.DateTimeField(auto_now_add=True)
+#
+#     chat = models.ForeignKey('Chat',
+#                              on_delete=models.CASCADE)
+#     user = models.ForeignKey('userface.CustomUser',
+#                              on_delete=models.SET_NULL,
+#                              null=True)
+#
+#     class Meta:
+#         ordering = ['date']
+#
+#
+# class Chat(models.Model):
+#     name = models.CharField(max_length=30)
+#     members = models.ManyToOneRel
